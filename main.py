888cdc2b198454df71f73d1d2121a71ae1d8e60d
omeedschool <135084489+omeedschool@users.noreply.github.com>
2023-06-09 09:25:33 +1200

    Version 1
    basic gui system

---
+++ main.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from tkinter import messagebox
+
+class MainWindow():
+    def __init__(self, root) -> None:
+        self.root = root
+        self.root.title("Main")
+
+        #Create labels and entry fields
+        self.label_full_name = Label(root, text="Full Name:")
+        self.label_full_name.grid(row=0, column=0, padx=5, pady=5)
+        self.entry_full_name = Entry(root)
+        self.entry_full_name.grid(row=0, column=1)
+
+        self.label_receipt_number = Label(root, text="Receipt Number:")
+        self.label_receipt_number.grid(row=1, column=0, padx=5, pady=5)
+        self.entry_receipt_number = Entry(root)
+        self.entry_receipt_number.grid(row=1, column=1)
+
+        self.label_item = Label(root, text="Item:")
+        self.label_item.grid(row=2, column=0, padx=5, pady=5)
+        self.entry_item = Entry(root)
+        self.entry_item.grid(row=2, column=1)
+
+        self.label_quantity = Label(root, text="Quantity:")
+        self.label_quantity.grid(row=3, column=0, padx=5, pady=5)
+        self.entry_quantity = Entry(root)
+        self.entry_quantity.grid(row=3, column=1)
+
+        #Create buttons
+        self.button_add = Button(root, text="Submit", command=self.add_item)
+        self.button_add.grid(row=4, column=0, padx=5, pady=5)
+
+        self.button_remove = Button(root, text="Return", command=self.return_item)
+        self.button_remove.grid(row=4, column=1, padx=5, pady=5)
+
+    def add_item(self):
+        pass
+
+    def return_item():
+        pass
+
+root = Tk()
+window = MainWindow(root)
+root.geometry('256x256')
+root.mainloop()
